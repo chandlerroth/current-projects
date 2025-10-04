fc4295576823cbe239e493129ba45c95433bf0ee
Chandler Roth <hey@chandlerroth.com>
2025-10-04 14:03:19 -0400

    add prj.sh for auto cd

---
+++ prj.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Shell integration for prj command
+# Add to ~/.zshrc or ~/.bashrc:
+#   source <(curl -s https://raw.githubusercontent.com/chandlerroth/current-projects/main/prj.sh)
+# Or download and source locally:
+#   curl -o ~/.prj.sh https://raw.githubusercontent.com/chandlerroth/current-projects/main/prj.sh
+#   source ~/.prj.sh
+
+prj() {
+  if [[ "$1" == "l" ]] || [[ "$1" == "list" ]]; then
+    local selected_path=$(command prj "$@")
+    if [[ -n "$selected_path" ]] && [[ -d "$selected_path" ]]; then
+      cd "$selected_path"
+    else
+      echo "$selected_path"
+    fi
+  elif [[ "$1" == "cd" ]]; then
+    local selected_path=$(command prj "$@")
+    if [[ -n "$selected_path" ]] && [[ -d "$selected_path" ]]; then
+      cd "$selected_path"
+    else
+      echo "$selected_path"
+    fi
+  else
+    command prj "$@"
+  fi
+}
